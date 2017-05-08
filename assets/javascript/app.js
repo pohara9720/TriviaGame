986ef4148b3942d6d02b57d2ca544ff0c23461f2
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-05-07 17:18:27 -0700

    56

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,74 @@
 $(document).ready(function() {
-var counter = 30;
-setInterval(timer(),1000);
+
+var timeOutQ1= setTimeout(nextQuestion1,5000);
 
 $("#Start").on("click", function(){
-$("#counter").html(timer());
 $("#Question").html("Why was the chainsaw first created?");
 $("#Start").addClass("hide");
 $("#options").removeClass("hide");
-$("#option1").html("Surgical Procedures");
-$("#option2").html("Cutting down trees faster");
-$("#option3").html("Hunting");
+$("#option1").html("Surgical Procedures").on("click",function(){
+	image1();
+	$("#Question").html("Correct! Chainsaws were originally created for surgical procedures");
+	$("#options").addClass("hide"); 
+	setTimeout(nextQuestion1,1000*6);
+	console.log(setTimeout);
+});
+$("#option2").html("Cutting down trees faster").on("click",function(){
+	image1();
+	$("#Question").html("WRONG! Chainsaws were originally created for surgical procedures");
+	$("#options").addClass("hide"); 
 
+});
+$("#option3").html("Hunting").on("click",function(){
+	image1();
+	$("#Question").html("WRONG! Chainsaws were originally created for surgical procedures");
+	$("#options").addClass("hide"); 
+});
+counter1();
 
 });
 
 function image1(){
 $("#image").attr("src", "assets/images/chainsaw.jpeg");
-}
+};
 
 function nextQuestion1(){
 $("#Question").html("What other industry did Apple try their hand in other then technology?");
+$("#options").removeClass("hide"); 
+$("#Start").addClass("hide");
 $("#option1").html("Food");
 $("#option2").html("Fashion");
 $("#option3").html("Film");
+counter1();
 };
+
 
 });
-function timer(){
-	counter--;
-	$("#counter").html(counter--);
 
-};
+
+counter1 = function(){
+
+(function(){
+  var countdown = 31;
+
+  setInterval(function() {
+    countdown--;
+    if (countdown >= 0) {
+      span = document.getElementById("counter");
+      span.innerHTML = countdown;
+    }
+    // Display 'counter' wherever you want to display it.
+    if (countdown === 0) {
+    	
+        clearInterval(counter);
+    }
+
+  }, 1000);
+
+})();
+
+}
+// setTimeout(func,timeout time );
 
 // 2why was the chain saw orinally created? Surgical procedures* / Chopping trees/ slicing bread
 
